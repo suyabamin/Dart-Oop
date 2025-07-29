1b0ddeb2f84846a0921d60d75309c89eb0886c6e
suyab-amin <suyabamins@gmail.com>
2025-07-29 15:16:27 +0600

    added constructer ,parameter constructer, this key word

--- MyClass.dart
+++ MyClass.dart
@@ -1,7 +1,7 @@
 
 class Myclass {
 
-/*var Name="my name is suyab amin sunny";
+var Name="my name is suyab amin sunny";
 var alphabet=['a','b','c','d','e'];
 
 void addTwoNum(a,b){
@@ -12,13 +12,13 @@ void addTwoNum(a,b){
   print("the sum of three number is="+(a+b+c).toString());
 }
 
-*/
+
 
 
 
 
 ///static
-static var Name="my name is suyab amin sunny";
+/*static var Name="my name is suyab amin sunny";
 static var alphabet =['a','b','c','d','e'];
 
 static addTwoNum(a,b){
@@ -27,6 +27,25 @@ static addTwoNum(a,b){
 
 static addThreeNum(a,b,c){
   print("the sum of three number is="+(a+b+c).toString());
+}*/
+
+//constructure
+Myclass(String msg){
+  print(msg);
 }
+
+///thes key word number
+var a=10;
+var b=20;
+var c=30;
+var d=40; 
+   addfourNumber(){
+    var res=this.a+this.b+this.c+this.d;
+    print("this sum is"+res.toString());
+   }
+   myFunction(){
+    this.addfourNumber();
+   }
+
 
 }
--- main.dart
+++ main.dart
@@ -3,24 +3,29 @@ import 'MyClass.dart';
 void main(){
   print("hello world");
 
-/*
-  var obj=new Myclass();
+
+  var obj=new Myclass("this is constructer");
   print(obj.Name);
   print(obj.alphabet);
   print(obj.alphabet[0]);
 
   obj.addTwoNum(2,4);
   obj.addThreeNum(1, 2, 5);
-*/
+
 
 
   ///static variable ,mathod call
-  
+  /*
+
   print(Myclass.Name);
   print(Myclass.alphabet);
   print(Myclass.alphabet[0]);
 
   Myclass.addTwoNum(2,4);
   Myclass.addThreeNum(1, 2, 5);
-  
+  */
+
+///use of this key word related function call
+obj.myFunction();   
+
   }
